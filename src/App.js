a93c83262eebac6e228ad02d87804834ee677e80
Er. Debraj Ray <debraj1990@gmail.com>
2018-10-29 12:56:32 +0530

    1) Added font-awesome style url in public/index.html 2) welcome speech in react app + mic icon in "App.css", "App.js"

--- src/App.js
+++ src/App.js
@@ -5,14 +5,61 @@ import PhraseMatcherService from './utils/phrase-matcher-service';
 import SpeechSynthesiserService from './utils/speech-synthesiser-service';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.speechApi= new SpeechSynthesiserService();
+    this.speechListenApi= new PhraseMatcherService();
+    this.helloVocalText = 'Click and Say "Hello React Baba"';
+    this.welcomeText = 'React Baba welcomes you - Meet the talking React app!!';
+    this.voiceSettingsComplete = this.speechApi.populateVoiceSettings();
+    this.voiceSettingsComplete
+    .then(
+      (val) => {console.log(val);
+        this.speechApi.speakText(this.welcomeText);
+        this.speechApi.speakText("To start, " + this.helloVocalText);
+      })
+      .catch(
+        (val) => {console.log(val);
+          this.speechApi.speakText(this.welcomeText);
+          this.speechApi.speakText("To start, " + this.helloVocalText);
+        });
+   }
   speakToManoranjan(event) {
     // this.speechApi.stopSpeaking();
     let target = event.target || event.srcElement || event.currentTarget;
     target.classList.add("in-use");
-    PhraseMatcherService.listenRealTimeSpeech(event, this, false, this.commandToActionConnector);
+    console.log(this);
+    this.speechListenApi.listenRealTimeSpeech(event, this, false, this.commandToActionConnector);
+  }
+  commandToActionConnector(targetElement, callerReference, voiceResponseObj) {
+    console.log('commandToActionConnector called..');console.log(voiceResponseObj);
+    targetElement.classList.remove("in-use");
+    if(voiceResponseObj.hasOwnProperty("speechResult")) {
+      console.log(voiceResponseObj["speechResult"]);
+      // console.log("Initial inputSpeech:"+callerReference.inputSpeech);
+      // targetElement.parentElement.parentElement.nextElementSibling.firstElementChild.firstElementChild.textContent = 'I heard "' + voiceResponseObj["speechResult"] + '"';
+      // console.log("after assignment inputSpeech:"+callerReference.inputSpeech);
+      
+      switch(voiceResponseObj["speechResult"].toUpperCase()) {
+        case "HELLO MANORANJAN":
+          callerReference.speechApi.speakText(callerReference.searchLabel);
+          // callerReference.manoranjanSays(callerReference.searchLabel);
+          break;
+        case "SEARCH MANORANJAN":
+          // callerReference.openSearchDialog();
+          // callerReference.modalService.open(SearchcriteriaComponent, {centered: true, backdropClass: 'light-blue-backdrop'});
+          targetElement.previousElementSibling.click();
+          break;
+        default:
+          callerReference.speechApi.speakText(callerReference.speechApi.invalidCommandMsg);
+          // callerReference.manoranjanSays(callerReference.speechApi.invalidCommandMsg);
+          break;
+      // callerReference.form.get('email').setValue("example@example.com");
+      // targetElement.value = callerReference.artistname;
+      }
+    }
   }
   render() {
-    const helloVocalText = 'Click and Say "Hello React Baba"';
     return (
       <div className="App">
         <header className="App-header">
@@ -30,9 +77,9 @@ class App extends Component {
           </a> */}
             <div className="col-sm align-content">            
                 {/* <button className="btn btn-primary btn-lg hide" title="{searchLabel}" onClick="openSearchDialog()">Search Artist</button> */}
-                <i className="fa fa-microphone" title={SpeechSynthesiserService.micLabel} onClick={this.speakToManoranjan}></i>
-                <div className="heading-text">{helloVocalText}
-                  {/* <i className="fa fa-info-circle" title="Info" onClick={SpeechSynthesiserService.speakText(helloVocalText)}></i> */}
+                <i className="fa fa-microphone" title={this.speechApi.micLabel} onClick={this.speakToManoranjan}></i>
+                <div className="heading-text">{this.helloVocalText}
+                  {/* <i className="fa fa-info-circle" title="Info" onClick={this.speechApi.speakText(helloVocalText)}></i> */}
                 </div>
               
             </div>   
