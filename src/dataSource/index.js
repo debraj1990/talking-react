3f28f9671d4017ea72a9409d9f8fa7e87d5de4af
Er. Debraj Ray <debraj1990@gmail.com>
2018-10-31 06:55:52 +0530

    Added dataSource/index.js

---
+++ src/dataSource/index.js
@@ -0,0 +1,77 @@
+const QuizQuestions = [
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : "",
+        "answer" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+    {
+        "question" : "",
+        "imgUrl" : "",
+        "optionA" : "",
+        "optionB" : "",
+        "optionC" : "",
+        "optionD" : ""
+    },
+];
+
+export default QuizQuestions;
